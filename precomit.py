af5fdfc2fdd88c26e813a6567443516ba9261cb6
Kurizu <83665497+crizmo@users.noreply.github.com>
2022-08-22 22:59:19 +0530

    🔮 : copy emoji from cli
    linux moment

---
+++ precomit.py
@@ -0,0 +1,50 @@
+import pyperclip as pc
+
+def main():
+    unicode = {
+        "update": "✨",
+        "fix": "🔨",
+        "patch": "🤝",
+        "merge": "⚡",
+        "security": "🔒",
+        "bug": "🐛",
+        "label": "🏷️",
+        "legal": "📝",
+        "docs": "📚",
+        "test": "🔬",
+        "build": "🔧",
+        "deploy": "🚀",
+        "release": "🎉",
+        "restructure": "🔥",
+        "beautify": "🔮",
+        "package": "📦",
+        "license": "🔑",
+        "minor_changes": "📍",
+    }
+
+    print("\n")
+    print("Precomit Dictionary")
+    print("----------------")
+
+    for key, value in unicode.items():
+        print(key, ":", value)
+    print("\n")
+
+    print("Enter the name of the emoji you want to copy to the clipboard")
+    print("Enter 'quit' to exit the program")
+    print("\n")
+
+    while True:
+        emoji = input("Emoji: ")
+        if emoji == "quit":
+            break
+        elif emoji in unicode:
+            pc.copy(unicode[emoji])
+            print("Copied emoji", unicode[emoji], "to clipboard")
+        else:
+            print("Emoji not found")
+
+
+
+if __name__ == "__main__":
+	main()
